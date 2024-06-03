e6ca360d055d8837ef6693051a7b79d921054b71
jeongyun-kim <kkanyo04@naver.com>
2024-06-03 18:09:58 +0900

    섹션 별 헤더 및 세팅 목록 enum 활용해서 보여주기

--- TableView0523/SettingTableViewController.swift
+++ TableView0523/SettingTableViewController.swift
@@ -7,13 +7,22 @@
 
 import UIKit
 
-class SettingTableViewController: UITableViewController {
-
-    // 섹션 별 헤더에 넣을 타이틀들
-    let headerTitles: [String] = ["전체 설정", "개인 설정", "기타"]
-    // 셀에 넣을 데이터 list[section][row]
-    let list = [["공지사항", "실험실", "버전 정보"], ["개인/보안", "알림", "채팅", "멀티프로필"], ["고객센터/도움말"]]
+// (0603) 헤더, 셀 내 아이템들 enum 활용하기
+enum SettingOptions: String, CaseIterable {
+    case all = "전체 설정" // 0
+    case personal = "개인 설정" // 1
+    case etc = "기타" // 2
     
+    var options: [String] {
+        switch self {
+        case .all: return ["공지사항", "실험실", "버전 정보"]
+        case .personal: return ["개인/보안", "알림", "채팅", "멀티프로필"]
+        case .etc: return ["고객센터/도움말"]
+        }
+    }
+}
+
+class SettingTableViewController: UITableViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         navigationItem.title = "설정"
@@ -21,17 +30,21 @@ class SettingTableViewController: UITableViewController {
     
     // 섹션 별 헤더 타이틀 세팅
     override func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
-        return headerTitles[section]
+        // 섹션 개수가 3개이므로 0, 1, 2
+        // -> allCases[0] = .all
+        // -> rawValue = "전체 설정"
+        return SettingOptions.allCases[section].rawValue
     }
     
     // 섹션의 개수
     override func numberOfSections(in tableView: UITableView) -> Int {
-        return headerTitles.count
+        return SettingOptions.allCases.count
     }
     
     // 각 섹션 내 행의 개수
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return list[section].count
+        // 각 섹션 타이틀이 불러오는 옵션들의 개수만큼
+        return SettingOptions.allCases[section].options.count
     }
     
     // 셀의 높이 조정
@@ -48,11 +61,9 @@ class SettingTableViewController: UITableViewController {
     
     // 섹션에 따라 셀에 각 다른 데이터 넣기
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        guard let cell = tableView.dequeueReusableCell(withIdentifier: "SettingCell") else { return UITableViewCell() }
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "SettingTableViewCell") else { return UITableViewCell() }
         cell.textLabel?.font = UIFont.systemFont(ofSize: 14)
-        cell.textLabel?.text = list[indexPath.section][indexPath.row]
+        cell.textLabel?.text = SettingOptions.allCases[indexPath.section].options[indexPath.row]
         return cell
-        
     }
-
 }
